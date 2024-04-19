b4c635965ed4cb1cb2df8d48c43c17ef17aff10f
Laura Dumitru <lauradumitru88@icloud.com>
2024-04-19 14:21:59 +0100

    Set the description to be hidden on page load and added event listener

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 lucide.createIcons();
+
+const readMore = Array.from(document.querySelectorAll(".myButton"));
+const descriptions = Array.from(document.querySelectorAll(".description"));
+
+descriptions.forEach((description) => {
+  description.style.display = "none";
+});
+
+readMore.forEach((button, index) => {
+  button.addEventListener("click", function () {
+    console.log("click");
+    descriptions[index].style.display = "block";
+  });
+});
