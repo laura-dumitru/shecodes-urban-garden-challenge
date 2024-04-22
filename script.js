3b1361a30658da55d5820c194c15008bf9221446
Laura Dumitru <lauradumitru88@icloud.com>
2024-04-22 15:19:41 +0100

    Created arrays

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ lucide.createIcons();
 
 const readMore = Array.from(document.querySelectorAll(".myButton"));
 const descriptions = Array.from(document.querySelectorAll(".description"));
-const descriptionsArray = ["Image 1 Barcelona", "Image 1 Barcelona", "Image 1 Barcelona"];
+const descriptionsArray = [
+  "Overlooking southwest Barcelona, Montjuic Park is the city’s green hilltop getaway, packed with history and attractions. Montjuïc is not only a park, it’s also a very prominent hill located right next to the Mediterranean. Archaeologists and historians believe it to be the birthplace of Barcelona. The park provides various cultural landmarks, such as The National Palace, The Magic Fountain..",
+  "Park Güell, Barcelona's iconic landmark, captivates visitors with its mesmerizing architecture crafted by the visionary artist Antoni Gaudí. Offering breathtaking panoramas of the cityscape, the park mesmerizes with vibrant mosaic artworks that adorn its pathways, drawing your gaze in every direction.",
+  "Located near the city center, this expansive park boasts lush greenery, serene ponds, and impressive sculptures. It's a favorite spot for picnics, leisurely walks, and outdoor activities.",
+];
+const parkArray = ["Montjuic Park", "Güell Park", "Parc de la Ciutadella"];
 
 descriptions.forEach((description) => {
   description.style.display = "none";
